2362c2c0fd0b3855f621b09ac07c60a837b80f26
kiwi-fruitiwi <zane.tian@gmail.com>
2022-04-13 13:32:03 -0700

    add general solution for pop segment i

--- codewriter.py
+++ codewriter.py
@@ -8,6 +8,34 @@ class CodeWriter:
         # try output.writelines! writes an array of strings
 
     def writePushPop(self, command):
+        """
+        general solution for pop segment i
+            @i
+            D=M
+            @seg
+            D=D+M       D=RAM[seg]+i
+
+            @popDest
+            M=D         put RAM[seg]+i into popDest variable
+
+            @SP
+            M=M-1       popping from the stack means decrementing SP
+
+            A=M
+            D=M         pattern for de-referencing: 'value at this RAM location'
+                        D ← RAM[value of SP]
+                        this is what we are popping
+
+            @popDest
+            M=D         put popped value into RAM[seg]+i
+
+
+        general solution for push segment i
+
+
+        :param command:
+        :return:
+        """
         self.output.write(f'writing asm code that implements {command}\n')
 
     def close(self):
