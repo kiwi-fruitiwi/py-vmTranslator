cb100cf5297ceecb1993aa1c0ef029020c0cd77f
kiwi-fruitiwi <zane.tian@gmail.com>
2022-04-14 12:39:00 -0700

    add push pop procreate diagram. revise comments for push pop in codewriter

--- codewriter.py
+++ codewriter.py
@@ -4,52 +4,55 @@ class CodeWriter:
 
     # writes to output file the asm commands that implement the vm command given
     def writeArithmetic(self, command):
+        # remember to add comments to each command!
+
         self.output.write(f'writing asm code that implements {command}\n')
         # try output.writelines! writes an array of strings
 
     def writePushPop(self, command):
         """
-        general solution for pop segment i
-            @i
-            D=M
-            @seg
-            D=D+M       D=RAM[seg]+i
+        remember to add comments to each command!
+        pseudocode: all commands in format of push/pop segName i
+            grab arg1 = seg, arg2 = i
 
-            @popDest
-            M=D         put RAM[seg]+i into popDest variable
+            pop segment i
+                @i
+                D=M         actually D=A if @i is a number instead of a variable
+                @seg
+                D=D+M       D=i+RAM[seg]
 
-            @SP
-            M=M-1       popping from the stack means decrementing SP
+                @popDest
+                M=D         put RAM[seg]+i into popDest variable
 
-            A=M
-            D=M         pattern for de-referencing: 'value at this RAM location'
-                        D ← RAM[value of SP]
-                        this is what we are popping
+                @SP
+                M=M-1       popping from the stack means decrementing SP
 
-            @popDest
-            M=D         put popped value into RAM[seg]+i
+                A=M
+                D=M         pattern for de-referencing:
+                            equivalent to 'value at this RAM location'
+                            D ← RAM[value of SP]
+                            this is what we are popping
 
+                @popDest
+                M=D         put popped value into RAM[seg]+i
 
-        general solution for push segment i
-            @i
-            D=M
-            @seg        seg is a value in some RAM location pointing to an addr
-            D=D+M       D ← segmentPointer + i
+            push segment i
+                @i
+                D=A
+                @seg        all segments are pointers to some addr in RAM
+                D=D+M       D ← segmentPointer + i
 
-            @addr
-            M=D         addr ← segmentPointer + i
-            A=M
-            D=M         D ← RAM[addr]
+                @addr
+                M=D         addr ← segmentPointer + i
+                A=M
+                D=M         D ← RAM[addr]
 
-            @SP
-            A=M         put RAM[SP] → A
+                @SP
+                A=M         put RAM[SP] → A
 
-            M=D         RAM[RAM[SP]] = RAM[addr], i.e. *SP=*addr
-            @SP
-            M=M+1       SP++; move the stack pointer forward one slot
-
-        :param command:
-        :return:
+                M=D         RAM[RAM[SP]] = RAM[addr], i.e. *SP=*addr
+                @SP
+                M=M+1       SP++; move the stack pointer forward one slot
         """
         self.output.write(f'writing asm code that implements {command}\n')
 
